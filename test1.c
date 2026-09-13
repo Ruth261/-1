767b70a5e2f2d765a4198a04f26bdc60414dbbed
Ruth261 <2776068143@qq.com>
2026-09-13 15:40:53 +0800

    提交test1

---
+++ test1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+	printf("tesint...\n");
+        printf("11111222");
+	printf("11111222");
+        printf("11111222");
+
+        printf("11111222");
+
+	printf("11111222");
+
+	return 0;
+}

0f93733f4eb0dc95a4a59aa4d76ab77e857a4c3a
Ruth261 <2776068143@qq.com>
2026-09-13 16:07:13 +0800

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 int main(void){
 	printf("tesint...\n");
-        printf("11111222");
-	printf("11111222");
-        printf("11111222");
-
-        printf("11111222");
-
-	printf("11111222");
-
 	return 0;
 }
